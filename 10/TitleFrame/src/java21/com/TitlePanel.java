bfbbfefa27835e9d378d5e4fd0e2c49d1f9a3df9
Karolina Lewińska <lewinskak95@gmail.com>
2021-02-07 11:05:56 +0100

    Add files via upload

---
+++ 10/TitleFrame/src/java21/com/TitlePanel.java
@@ -0,0 +1,28 @@
+package java21.com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TitlePanel extends JFrame {
+    public TitlePanel() {
+        super("Tytuł ramki");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLookAndFeel();
+        String iPannel = JOptionPane.showInputDialog(null, "Tytuł ramki","Wpisz tytuł ramki",
+                JOptionPane.PLAIN_MESSAGE);
+        setVisible(true);
+
+    }
+    private void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                    "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+            );
+            SwingUtilities.updateComponentTreeUI(this);
+        }catch(Exception exc) { }
+    }
+
+    public static void main(String[] args) {
+        TitlePanel panel1 = new TitlePanel();
+    }
+}
