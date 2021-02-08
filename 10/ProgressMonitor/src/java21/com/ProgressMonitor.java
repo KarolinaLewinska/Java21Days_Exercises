e4e5388ca8f1dfac492bee744a4fbba7ffae04ce
Karolina Lewińska <lewinskak95@gmail.com>
2021-02-08 12:57:23 +0100

    Add files via upload

---
+++ 10/ProgressMonitor/src/java21/com/ProgressMonitor.java
@@ -0,0 +1,55 @@
+package java21.com;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ProgressMonitor extends JFrame {
+    JProgressBar current;
+    int num = 0;
+    JTextField numField;
+
+    public ProgressMonitor() {
+        super("Monitorowanie postępu");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLookAndFeel();
+        setSize(250, 150);
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        numField = new JTextField("" + num, 3);
+        panel.add(numField);
+        current = new JProgressBar(0, 2000);
+        current.setValue(0);
+        current.setStringPainted(true);
+        panel.add(current);
+        setContentPane(panel);
+        setVisible(true);
+    }
+
+    public void iterate() {
+        while (num < 2000) {
+            current.setValue(num);
+            numField.setText("" + num);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) { }
+            num += 95;
+        }
+    }
+
+    private void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                    "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+            );
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception e) {
+            System.err.println("Nie potrafię wczytać "
+                    + "systemowego wyglądu: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        ProgressMonitor frame = new ProgressMonitor();
+        frame.iterate();
+    }
+}
