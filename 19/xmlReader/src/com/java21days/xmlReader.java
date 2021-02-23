197dea3d79e4ee7121d49bf09e21b56a9e9255db
Karolina Lewińska <lewinskak95@gmail.com>
2021-02-23 07:37:02 +0100

    Add files via upload

---
+++ 19/xmlReader/src/com/java21days/xmlReader.java
@@ -0,0 +1,34 @@
+package com.java21days;
+
+import java.io.*;
+import nu.xom.*;
+
+public class xmlReader {
+     try {
+        Builder builder1 = new Builder();
+        File xml = new File("product-report.xml");
+        Document doc = builder.build(xml);
+
+        Element root = doc.getRootElement();
+
+        Elements products = root.getChildElements("product");
+        for (int i = 0; i < products.size(); i++) {
+            Element product = products.get(i);
+            Element productID = product.getFirstChildElement("productID");
+            Element productCode = product.getFirstChildElement("productCode");
+            Element purchaseCost = product.getFirstChildElement("purchaseCost");
+            System.out.println("ID: " + productID.getValue());
+            System.out.println("Kod produktu: " + productCode.getValue());
+            System.out.println("Koszt zakupu: " + purchaseCost.getValue());
+            System.out.println();
+        }
+    } catch (ParsingException | IOException e) {
+        System.out.println("Błąd: " + e.getMessage());
+        e.printStackTrace();
+    }
+
+    public static void main(String[] args) {
+        xmlReader reader1 = new xmlReader();
+
+    }
+}
