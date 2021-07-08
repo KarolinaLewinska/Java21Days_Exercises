cb0b4f7fd1b6afff51088511ff343fc8b151a6f7
Karolina Lewińska <lewinskak95@gmail.com>
2021-07-08 08:31:31 +0200

    Update Main.java

--- 3/Wardrobe/src/java21/com/Main.java
+++ 3/Wardrobe/src/java21/com/Main.java
@@ -3,11 +3,11 @@ package java21.com;
 public class Main {
 
     public static void main(String[] args) {
-        Wardrobe model1 = new Wardrobe(20,30,15);
+        Wardrobe model1 = new Wardrobe(20, 30, 15);
         model1.showParameters();
         System.out.println();
 
-        Wardrobe model2 = new Wardrobe(11,12,10);
+        Wardrobe model2 = new Wardrobe(11, 12, 10);
         model2.showParameters();
     }
 }
