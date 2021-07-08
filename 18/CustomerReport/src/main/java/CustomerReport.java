c9341f3ee7c66779fbe4829fb5cde315d9e6ea78
Karolina Lewińska <lewinskak95@gmail.com>
2021-07-08 10:57:28 +0200

    Update CustomerReport.java

--- 18/CustomerReport/src/main/java/CustomerReport.java
+++ 18/CustomerReport/src/main/java/CustomerReport.java
@@ -3,11 +3,9 @@ import java.sql.*;
 public class CustomerReport {
    
     public static void main(String[] args) {
-        String data = "jdbc:derby://localhost:1527/sample";
-       try (
-           Connection conn = DriverManager.getConnection(
-               data, "app", "APP");
-           Statement st = conn.createStatement()) {
+       String data = "jdbc:derby://localhost:1527/sample";
+       try (Connection conn = DriverManager.getConnection(data, "app", "APP");
+            Statement st = conn.createStatement()) {
 
            Class.forName("org.apache.derby.jdbc.ClientDriver");
 
@@ -15,7 +13,8 @@ public class CustomerReport {
                 "select PRODUCT_ID, PRODUCT_CODE, PURCHASE_COST"
                         + "from APP.PRODUCT " +
                 "order by PRODUCT_ID");
-            while (rec.next()) {
+            
+          while (rec.next()) {
                 System.out.println("CUSTOMER_ID:\t"
                     + rec.getString(1));
                 System.out.println("NAME:\t" + rec.getString(2));
@@ -24,6 +23,7 @@ public class CustomerReport {
                 System.out.println();
             }
             st.close();
+          
         } catch (SQLException s) {
             System.out.println("Błąd SQL: " + s.toString() + " "
                 + s.getErrorCode() + " " + s.getSQLState());
@@ -32,5 +32,4 @@ public class CustomerReport {
                 + e.getMessage());
         }
     }
-    
 }
