d5fc5ad2b2431e22f1e3294a4f6bf6b64a74c9d8
Karolina Lewińska <lewinskak95@gmail.com>
2021-07-08 09:17:12 +0200

    Update Calendar.java

--- 11/Calendar/src/java21/com/Calendar.java
+++ 11/Calendar/src/java21/com/Calendar.java
@@ -4,21 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Calendar extends JFrame {
+    JLabel number;
     JLabel[] daysNames = new JLabel[7];
     String[] namesOfDays = {"Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"};
-    JLabel number;
+	
     public Calendar() {
         super("Kalendarz na luty");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         GridLayout lay = new GridLayout(5,7,10,10);
-        panel.setLayout(lay);
+        panel.setLayout(lay); 
         for (int i = 0; i < 7; i++) {
             daysNames[i] = new JLabel(namesOfDays[i]);
             panel.add(daysNames[i]);
-        }
+        }  
         for (int i = 0; i < 28; i++) {
-            number = new JLabel("" + (i+1));
+            number = new JLabel("" + (i + 1));
             panel.add(number);
         }
         add(panel);
@@ -29,11 +30,11 @@ public class Calendar extends JFrame {
 
     private void setLookAndFeel() {
         try {
-            UIManager.setLookAndFeel(
-                    "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
-            );
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
             SwingUtilities.updateComponentTreeUI(this);
-        }catch (Exception exc) { }
+        } catch (Exception exc) {
+		
+	}
     }
 
     public static void main(String[] args) {
