1e7604a940b5fc3617926facf24e57d5004976c7
Karolina Lewińska <lewinskak95@gmail.com>
2021-07-08 11:02:55 +0200

    Update xmlCreator.java

--- 19/xmlCreator/src/com/java21days/xmlCreator.java
+++ 19/xmlCreator/src/com/java21days/xmlCreator.java
@@ -8,31 +8,27 @@ public class xmlCreator {
 
     public static void main(String[] args) {
         String db = "jdbc:derby://localhost:1527/sample";
-        try (
-                Connection conn = DriverManager.getConnection(
-                        db, "app", "APP");
-                Statement statement = conn.createStatement()) {
+        try (Connection conn = DriverManager.getConnection(db, "app", "APP");
+            Statement statement = conn.createStatement()) {
 
             Class.forName("org.apache.derby.jdbc.ClientDriver");
-
             Element root = new Element("products");
-
+            
             ResultSet query = statement.executeQuery(
                     "select PRODUCT_ID, PRODUCT_CODE, PURCHASE_COST " +
                             "from APP.PRODUCT " +
                             "order by PRODUCT_ID");
+            
             while (query.next()) {
                 Element product = new Element("product");
-
                 Element id = new Element("product_id");
-
                 id.appendChild(query.getString(1));
-
                 product.appendChild(id);
-
+                
                 Element productCode = new Element("productCode");
                 productCode.appendChild(query.getString(3));
                 product.appendChild(productCode);
+                
                 Element purchaseCost = new Element("purchaseCost");
                 purchaseCost.appendChild(query.getString(4));
                 product.appendChild(purchaseCost);
