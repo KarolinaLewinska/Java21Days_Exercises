963650c6d15af7d87af24c7f8e00759141df8d8e
Karolina Lewińska <lewinskak95@gmail.com>
2021-07-08 08:46:02 +0200

    Update ZipCode.java

--- 6/ZipCode/src/java21/com/ZipCode.java
+++ 6/ZipCode/src/java21/com/ZipCode.java
@@ -6,11 +6,11 @@ public class ZipCode {
     public ZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
-    public void validateZipCode(){
-        if(zipCode.length()==6 || zipCode.contains("-")){
+    
+    public void validateZipCode() {
+        if (zipCode.length() == 6 || zipCode.contains("-")) 
             System.out.println("Zip Code: " + zipCode);
-        }else{
+        else
             System.out.println("Zip Code has incorrect format!");
-        }
     }
 }
