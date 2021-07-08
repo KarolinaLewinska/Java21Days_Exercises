fa1d49a3b8dc1899a3f2a1fd8d3c81bb3a3ff3e5
Karolina Lewińska <lewinskak95@gmail.com>
2021-07-08 09:48:26 +0200

    Update FileReaderAndZeroWriter.java

--- 15/FileReaderAndZeroWriter/src/com/java21/FileReaderAndZeroWriter.java
+++ 15/FileReaderAndZeroWriter/src/com/java21/FileReaderAndZeroWriter.java
@@ -5,12 +5,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileReaderAndZeroWriter {
-
     public static void main(String[] args) {
         try {
             FileInputStream file = new FileInputStream("file.txt");
             boolean isTrue = true;
             int sizeOfFile = 0;
+            
             while (isTrue) {
                 int input = file.read();
                 if (input == -1) {
@@ -23,7 +23,7 @@ public class FileReaderAndZeroWriter {
 
             FileOutputStream file2 = new FileOutputStream("file.txt");
             for (int i = 0; i < sizeOfFile; i++) {
-                file2.write( (byte) 0 );
+                file2.write((byte) 0);
             }
             file2.close();
         } catch (IOException e) {
